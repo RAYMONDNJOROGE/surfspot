350e6d40ca562280c6d21481724aa3783c6d111f
RAYMONDNJOROGE <raymondnjoroge20@gmail.com>
2025-08-15 19:37:31 +0300

    gfdS

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = 'https://surfspot.onrender.com/api';
     const ADMIN_API_BASE_URL = `${API_BASE_URL}/admin`;
 
     // Utility function to show a modal
